09166ddc255113f8e493d54ef907fcf5b274f420
ramonsantos-ti <ramonsantos.ti@gmail.com>
2025-08-11 13:31:01 -0300

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -14,7 +14,6 @@ import { Avatar, AvatarFallback } from './ui/avatar';
 import { LogOut, User, Settings, Home } from 'lucide-react';
 import { SidebarProvider, SidebarTrigger } from './ui/sidebar';
 import { AppSidebar } from './AppSidebar';
-import { Link, useLocation } from "react-router-dom";
 import { BarChart3, Search } from "lucide-react";
 import {
   Sidebar,
